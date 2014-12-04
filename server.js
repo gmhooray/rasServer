810c109b1e558e718b9e33104ad3f4ec29646f31
gman <naver@naverui-MacBook-Air.local>
2014-12-05 00:21:49 +0900

    server

--- server.js
+++ server.js
@@ -1,11 +1,90 @@
+
+var cluster = require('cluster');
+var os = require('os');
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
-var port = Number(process.env.PORT || 20787);
+var port = Number(process.env.PORT || 7777);
+var numCPUs = os.cpus().length;
 
-var os = require('os');
-var ifaces = os.networkInterfaces();
+/* Node Child_process */
+var cp = require('child_process');
+
+if (cluster.isMaster) {
+    // Fork workers.
+    for (var i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('exit', function(worker, code, signal) {
+        console.log('worker ' + worker.process.pid + ' died');
+    });
+} else {
+    // Workers can share any TCP connection
+    // In this case its a HTTP server
+    var path = require('path');
+    app.use(express.static(path.join( __dirname , 'src/')));
+
+    app.get('/', function (req, res) {
+        res.sendFile(__dirname + '/index.html');
+    });
+
+    /* socket.io event */
+    io.on('connection', function(socket){
+        socket.emit('news', { hello: 'world' });
+        if( process.platform  == "win32" ){
+            socket.on('pushPrev', function(data){
+                gServer.cmd('./tool/WinSendKeys/WinSendKeys.exe',['-t','2000','-w','[ACTIVE]','{UP}']);
+            })
+            socket.on('pushNext', function(data){
+                gServer.cmd('./tool/WinSendKeys/WinSendKeys.exe',['-t','2000','-w','[ACTIVE]','{DOWN}']);
+            })
+        }else if(process.platform  == "darwin"){
+            socket.on('pushPrev', function(data) {
+                gServer.cmd('osascript',['-e', 'tell app \"System Events\" to key code 126']);
+            })
+            socket.on('pushNext', function(data){
+                gServer.cmd('osascript',['-e', 'tell app \"System Events\" to key code 125']);
+            })
+        }
+    })
+
+
+    var spawn = cp.spawn;
+
+    var gman = function(){this.$init.apply(this,arguments)};
+    gman.prototype = {
+        $init: function(){
+            this._sResult = '';
+        },
+        cmd : function (sCommand, aParameter){
+            this._cmd = spawn(sCommand, aParameter);
+
+            this._cmd.stdout.on('data', function (data) {
+                console.log('stdout: ' + data);
+            });
+
+            this._cmd.stderr.on('data', function (data) {
+                console.log('stderr: ' + data);
+            });
+
+            this._cmd.on('close', function (code) {
+                console.log('child process exited with code ' + code);
+            });
+        }
+    }
+
+    var gServer = new gman();
+
+    server.listen(port, function() {
+        console.log("Listening on " + port);
+    });
+}
+
+
+
+/*var ifaces = os.networkInterfaces();
 for (var dev in ifaces) {
     var alias=0;
     ifaces[dev].forEach(function(details){
@@ -14,63 +93,4 @@ for (var dev in ifaces) {
             ++alias;
         }
     });
-}
-
-/* Node Child_process */
-var cp = require('child_process');
-var spawn = cp.spawn;
-
-var path = require('path');
-app.use(express.static(path.join( __dirname , 'src/')));
-
-var gman = function(){this.$init.apply(this,arguments)};
-gman.prototype = {
-    $init: function(){
-        this._sResult = '';
-    },
-    cmd : function (sCommand, aParameter){
-        this._cmd = spawn(sCommand, aParameter);
-
-        this._cmd.stdout.on('data', function (data) {
-            console.log('stdout: ' + data);
-        });
-
-        this._cmd.stderr.on('data', function (data) {
-            console.log('stderr: ' + data);
-        });
-
-        this._cmd.on('close', function (code) {
-            console.log('child process exited with code ' + code);
-        });
-    }
-}
-
-var gServer = new gman();
-
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
-
-/* socket.io event */
-io.on('connection', function(socket){
-    socket.emit('news', { hello: 'world' });
-    if( process.platform  == "win32" ){
-        socket.on('pushPrev', function(data){
-            gServer.cmd('./tool/WinSendKeys/WinSendKeys.exe',['-t','2000','-w','[ACTIVE]','{UP}']);
-        })
-        socket.on('pushNext', function(data){
-            gServer.cmd('./tool/WinSendKeys/WinSendKeys.exe',['-t','2000','-w','[ACTIVE]','{DOWN}']);
-        })
-    }else if(process.platform  == "darwin"){
-        socket.on('pushPrev', function(data) {
-            gServer.cmd('osascript',['-e', 'tell app \"System Events\" to key code 126']);
-        })
-        socket.on('pushNext', function(data){
-            gServer.cmd('osascript',['-e', 'tell app \"System Events\" to key code 125']);
-        })
-    }
-})
-
-server.listen(port, function() {
-    console.log("Listening on " + port);
-});
+}*/
